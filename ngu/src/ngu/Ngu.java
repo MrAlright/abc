0139258cbeb269835792f36e74bbe1cd1a33f3d6
MrAlright <cerabustoon@gmail.com>
2016-06-05 21:23:05 +0700

    loz ma

--- ngu/src/ngu/Ngu.java
+++ ngu/src/ngu/Ngu.java
@@ -18,6 +18,7 @@ public class Ngu {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("tu ngu loz");
+        System.out.println("cc");
     }
     
 }
