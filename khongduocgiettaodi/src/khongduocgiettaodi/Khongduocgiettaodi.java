cdc007980b82f41ae2d2c7485bceee119da9c42c
MrAlright <cerabustoon@gmail.com>
2016-06-05 20:16:31 +0700

    abc 2

--- khongduocgiettaodi/src/khongduocgiettaodi/Khongduocgiettaodi.java
+++ khongduocgiettaodi/src/khongduocgiettaodi/Khongduocgiettaodi.java
@@ -17,6 +17,7 @@ public class Khongduocgiettaodi {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("tu sieu nhan");
     }
     
 }
